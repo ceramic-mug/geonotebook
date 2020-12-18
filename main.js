a292b4e3a96a75e69cd0400927fbd470f7e6f4ec
ceramic-mug <ceramic_mug@outlook.com>
2020-12-18 14:12:38 -0800

    YES

--- main.js
+++ main.js
@@ -33,43 +33,35 @@ MainWin = app.whenReady().then(createWindow)
 console.info("Main window created.")
 
 // create a folder to hold pois if note already created
-if (!fs.existsSync('./poi')) {fs.mkdir('./poi', function() {})}
+if (!fs.existsSync('./poi')) {
+	fs.mkdir('./poi', function() {});
+	
+}
 
-// read through the poi folder and add all saved pois to map
-ipcMain.on('load-pois', function(event) {
-    console.log('Loading Pois from Main Process')
-    fs.readdir('./poi/', function (err, folders) {
-	if (err) {
-	    return console.log('Unable to scan poi direcory: ' + err)
-	}
-	var pois = []
-	for (i = 0; i<folders.length; i++){
-		if (folders[i] == '.DS_Store') continue;
-		dir = './poi/'+folders[i]+'/'
-	    file = dir + 'head.txt'
-	    // send the file back for processing
-	    pois.push(file)
-	}
-	event.returnValue = pois
-	})})
+// Create GeoJson to hold all POIs if not in POI folder
 
-	/* ********************** */
 
-/* ******* POI Map Interactions ******* */
+
+
+
+// 	/* ********************** */
+
+// /* ******* POI Map Interactions ******* */
 ipcMain.on('map-click', function(event) {
-	console.log('Map clicked. Enter name for new POI or return NULL');
-	const nameWinHeight = 150;
-	const nameWinWidth = 400;
-	var nameWin = new BrowserWindow({
-		width: nameWinWidth,
-		height: nameWinHeight,
-		frame: false,
-		titleBarStyle: "hidden",
-		parent: MainWin,
-		webPreferences: {
-            nodeIntegration: true
-        }
-    })
-	nameWin.loadFile('./src/nameWin.html')
-	ipcMain.on('no-name', nameWin.close)
+	app.showEmojiPanel();
+	// console.log('Map clicked. Enter name for new POI or return NULL');
+	// const nameWinHeight = 150;
+	// const nameWinWidth = 400;
+	// var nameWin = new BrowserWindow({
+	// 	width: nameWinWidth,
+	// 	height: nameWinHeight,
+	// 	frame: false,
+	// 	titleBarStyle: "hidden",
+	// 	parent: MainWin,
+	// 	webPreferences: {
+    //         nodeIntegration: true
+    //     }
+    // })
+	// nameWin.loadFile('./src/nameWin.html')
+	// ipcMain.on('no-name', nameWin.close)
 })
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 /* imports */
 const L = require('leaflet');
+const twemoji = require('twemoji');
 const fs = require('fs');
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, app } = require('electron');
 
 /* ******** Constants ********* */
+
+const configFile = './src/config.json'
+const poiFile = './poi/poi.geojson'
+const iconDir = './img/svg/'
+let config = JSON.parse(fs.readFileSync(configFile)) /* can be edited, update at program shutdown */
+let pois = JSON.parse(fs.readFileSync(poiFile)) /* can be edited, update at program shutdown */
 
 const strMapObject = 'map'
 const tmpPoiImgURL = './img/tmp_newPoi.svg'
@@ -13,11 +20,14 @@ const layerMapbox = {
     'attribution': 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors,'+
 	'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     'maxZoom': 18,
-    'id': 'mapbox/streets-v11',
+    'id': 'ceramicmug/cke7lvu382bhs19o4l2aev08b',
     'tileSize': 512,
     'zoomOffset': -1,
-    'accessToken': 'pk.eyJ1IjoiY2VyYW1pY211ZyIsImEiOiJja2U3bG9nZngxbzN0MnhudDY0cjcxdmtuIn0.xIox5D1GCX_Sy-7ARCFtBg'
-};
+    // for personal styles
+    'accessToken': 'pk.eyJ1IjoiY2VyYW1pY211ZyIsImEiOiJja2U3bGxxaDAxbzE2MzFzempsb3Jkam9kIn0.BJX4awVLutkNLqHahwJOEw'
+    // for base mapBox (below)
+    // 'accessToken': 'pk.eyJ1IjoiY2VyYW1pY211ZyIsImEiOiJja2U3bG9nZngxbzN0MnhudDY0cjcxdmtuIn0.xIox5D1GCX_Sy-7ARCFtBg'
+};//
 
 const layer = {
     'mapbox' : layerMapbox
@@ -25,21 +35,15 @@ const layer = {
 
 const intComfyZoom = 13;
 
+const ICONSIZE = 50;
+
 const tmpIcon = L.icon({
     iconUrl: tmpPoiImgURL,
-    iconSize: [50, 50],
-    iconAnchor: [25, 25]
+    iconSize: [ICONSIZE, ICONSIZE],
+    iconAnchor: [ICONSIZE/2, ICONSIZE/2]
 })
 
 /* structures */
-
-/*
-   coords: Holds application-state coordinates, such as
-   for the location of the map on startup.
-*/
-var coords = {
-    'start' : [38.4192, -82.4452]
-};
 
 /* ************* REGEX ************* */
 
@@ -77,41 +81,73 @@ function popupText(t,d,c) {
 /* ************* RENDERER PROCESS  ************** */
 
 /* create the map */
-var map = L.map(strMapObject).setView(coords['start'], intComfyZoom);
-L.tileLayer(layer['mapbox']['url'], {
-    attribution: layer['mapbox']['attribution'],
-    maxZoom: layer['mapbox']['maxZoom'],
-    id: layer['mapbox']['id'],
-    tileSize: layer['mapbox']['tileSize'],
-    zoomOffset: layer['mapbox']['zoomOffset'],
-    accessToken: layer['mapbox']['accessToken']
+var map = L.map(strMapObject, {
+                zoomControl: true
+            }).setView(config.startCoords, intComfyZoom);
+L.tileLayer(        layer['mapbox']['url'], {
+    attribution:    layer['mapbox']['attribution'],
+    maxZoom:        layer['mapbox']['maxZoom'],
+    id:             layer['mapbox']['id'],
+    tileSize:       layer['mapbox']['tileSize'],
+    zoomOffset:     layer['mapbox']['zoomOffset'],
+    accessToken:    layer['mapbox']['accessToken']
 }).addTo(map);
 
+//add zoom control with your options
+map.zoomControl.setPosition('topright');
+
+// TODO: use layers instead of adding directly to enable adding/removing/editing markers
 // add saved pois to map
-pois = ipcRenderer.sendSync('load-pois')
+let poiLen = pois.features.length;
+for (i=0; i < poiLen; i++){
+    // get the feature
+    let poi = pois.features[i]
+    // get the twemoji code for the emoji
+    let ic = twemoji.convert.toCodePoint(poi['properties']['icon'])
+    // add the '.svg' to it
+    let icnUrl = iconDir.concat(ic,'.svg')
+    // create the icon object
+    let icon = L.icon({
+        iconUrl: icnUrl,
+        iconSize: [50, 50]
+    })
+    // create the marker and add it to the map
+    let m = L.marker(
+        poi['geometry']['coordinates'],
+        {icon : icon}
+    ).addTo(map)
+    m.bindPopup(popupText(
+        poi['properties']['title'],
+        poi['properties']['description'],
+        poi['geometry']['coordinates']
+    ))
+}
+
+// for getting various emoji codes // TODO: remove for release
+console.log(twemoji.convert.toCodePoint('📂'))
 
 // iterate through returned list of files
-pois.forEach( function (file) {
+// pois.forEach( function (file) {
 
-    // read the file, root out errors, and place it on the map with popup info
-    fs.readFile(file, 'utf8', function(err, data) {
-	if (err) {
-	    return console.log(err)
-    }
+//     // read the file, root out errors, and place it on the map with popup info
+//     fs.readFile(file, 'utf8', function(err, data) {
+// 	if (err) {
+// 	    return console.log(err)
+//     }
 
-	// call regular expression on the contents of the header file
-	info = reHead.exec(data)
-	var latlng = L.latLng(info[2],info[3])
-	var title = reHead.exec(data)[1]
-	var date = reHead.exec(data)[4]
+// 	// call regular expression on the contents of the header file
+// 	info = reHead.exec(data)
+// 	var latlng = L.latLng(info[2],info[3])
+// 	var title = reHead.exec(data)[1]
+// 	var date = reHead.exec(data)[4]
 
-	// Set marker and add text to popup
-	var marker = L.marker(latlng).addTo(map)
-	ll = latlng.toString().replace('LatLng(','').replace(')','')
-	marker.bindPopup(popupText(title,date,ll));
+// 	// Set marker and add text to popup
+// 	var marker = L.marker(latlng).addTo(map)
+// 	ll = latlng.toString().replace('LatLng(','').replace(')','')
+// 	marker.bindPopup(popupText(title,date,ll));
 
-    })
-})
+//     })
+// })
 
 // listen for clicks on the map and add POI
 document.addEventListener('click', map.on('click', onMapClick))
@@ -126,46 +162,45 @@ document.addEventListener('click', map.on('click', onMapClick))
 //      -> If no, delete temporary layer and return 0
 
 function onMapClick(e) {
-    map.panTo( // step 1
-        e.latlng,
-        Animation=true
-    )
+    map.setView(e.latlng, map.getZoom(), { // center view
+        "animate": true,
+        "pan": {
+          "duration": 0.5
+        }
+      });
     
     // create temporary marker
     curLat = e.latlng.lat
     curLong = e.latlng.long
 
+    // create the icon object
+    let tmpIcon = L.icon({
+        iconUrl: iconDir.concat(twemoji.convert.toCodePoint(config['defaultIcon']),'.svg'),
+        iconSize: [50, 50],
+        tooltipAnchor: [0, -10],
+    })
     tmpMarker = L.marker(e.latlng, {icon: tmpIcon})
     tmpMarker.addTo(map)
 
-    // prompt user to add location or not
-    addPrompt = document.createElement("div")
-
-    yesButton = document.createElement("button")
-    yesButton.setAttribute("id","yesButton")
-    yesButton.setAttribute("id","yesButton")
-    yesButton.innerHTML = "&check;"
-
-    noButton = document.createElement("button")
-    noButton.setAttribute("id","noButton")
-    noButton.innerHTML = "&cross;"
-
-    addPrompt.innerHTML = "Save location?" 
-    addPrompt.appendChild(yesButton)
-    addPrompt.appendChild(noButton)
-
-    document.body.insertBefore(addPrompt, document.getElementById('map'))
-
-    // TODO: get prompt buttons to show up and work properly
+    tmpMarker.bindTooltip(
+        e.latlng.toString().replace('LatLng(','').replace(')','') + "<br>",
+        {
+            permanent: true
+        }
+    )
 
     // ipcRenderer.sendSync('map-click')
+
+    // TODO: Conditionally save marker and add title, description, etc.
+    //          if save, add marker to poi
+    //          CHOOSE ~20 emoji icons
+    //          *remember to rewrite pois.geojson at SIGKILL
 }
 
 // Managing sidebar actions
 
 sidebarButton = document.getElementById("sidebarButton")
 sidebar =  document.getElementById("sidebar")
-
 sidebarButton.addEventListener("click",openSideBar)
 
 // Opens the sidebar (left)
